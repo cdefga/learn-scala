7edfbb07febdc568d772e30ccb23d0cbca18f37a
cdefga <hyouka266@gmail.com>
2021-01-25 10:17:44 +0700

    List in scala

--- Main.scala
+++ Main.scala
@@ -45,5 +45,16 @@ object Main extends App {
     // println(res)
 
     def filter(p: Int => Boolean, x: Int): AnyVal = if (p(x)) x else false
-    println(filter(x => x > 3, 4))
+    // println(filter(x => x > 3, 4))
+
+    def test_list(list: List[Int]): Unit = {
+        println(s"length: ${list.length}")
+        println(s"last: ${list.last}")
+        println(s"init: ${list.init}")
+        println(s"tail: ${list.tail}")
+        println(s"take 3 : ${list take 3}")
+        println(s"drop 3 : ${list drop 3}")
+        println(s"apply 3 : ${list apply 3} ${list(3)}")
+    }
+    test_list(List(1,2,3,4))
 }
